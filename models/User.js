d8406bd7df7126cb1e6eaa5343ec86c2c23cc4e0
maschzh <maschzh_001@163.com>
2015-03-03 08:52:07 +0800

    first commit

---
+++ models/User.js
@@ -0,0 +1,5 @@
+function User(user){
+	this.name=user.name;
+	this.password=user.password;
+};
+module.exports=User;
---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports={
+	cookie_secret: 'secret_meteoric',
+	db:'cooks',
+	host:'localhost',
+	port:27017
+}
