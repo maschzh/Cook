e1d9ea2ca3b0d4ea30e92e65786e0071033cc9e8
chenzhic <chenzhic@yonyou.com>
2020-02-27 08:52:12 +0800

    修改

--- app.js
+++ app.js
@@ -28,8 +28,22 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
+app.all('/*', function(req, res, next) {
+    // CORS headers
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    // Set custom headers for CORS
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    if (req.method == 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
+});
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.post('/encrypt', user.encrypt);
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,32 @@
-
 /*
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+const JSEncrypt = require('node-jsencrypt');
+
+exports.list = function(req, res) {
+    res.send("respond with a resource");
+};
+
+exports.encrypt = function(req, res) {
+    let param = req.body;
+    try {
+        let data = _encrypt(param.data, param.publicKey);
+        res.json({
+            Data: data,
+            Error: null
+        });
+    } catch (error) {
+        res.json({
+            Data: null,
+            Error: error
+        });
+    }
+
+};
+
+function _encrypt(data, publicKey) {
+    let encrypt = new JSEncrypt();
+    encrypt.setPublicKey(publicKey);
+    return encrypt.encrypt(data);
 };
